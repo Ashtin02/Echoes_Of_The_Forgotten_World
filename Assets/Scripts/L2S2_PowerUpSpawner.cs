c5ddd2d665c519030311291250202dba4ceb748b
Ashtin <145617836+Ashtin02@users.noreply.github.com>
2025-05-15 18:39:23 -0700

    Made Power-Ups Fall from top of screen adn you catch them to activate

---
+++ Assets/Scripts/L2S2_PowerUpSpawner.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.Intrinsics;
+using UnityEngine;
+
+public class L2S2_PowerUpSpawner : MonoBehaviour
+{
+    public GameObject Heal;
+    public GameObject Shield;
+    public GameObject Piercing;
+    public GameObject SpeedBoost;
+    public GameObject DoubleShot;
+    public GameObject TripleShot;
+    public GameObject ExtraLife;
+    public GameObject SlowTime;
+    public GameObject Nuke;
+    public GameObject Ghost;
+    public GameObject Shrink;
+    public GameObject Grow;
+
+    public float spawnRate = 30f;
+    public float minX = -8f;
+    public float maxX = 8f;
+    public float spawnY = 9f;
+
+    public List<GameObject> PowerUps;
+
+    private void Start()
+    {
+        CreateWeightedList();
+        StartCoroutine(SpawnPowerUps());
+
+
+    }
+
+    private void CreateWeightedList()
+    {
+        PowerUps = new List<GameObject>();
+
+        AddPowerUps(Heal, 20);
+        AddPowerUps(SpeedBoost, 18);
+        AddPowerUps(DoubleShot, 15);
+        AddPowerUps(Shield, 15);
+        AddPowerUps(Piercing, 10);
+        AddPowerUps(TripleShot, 10);
+        AddPowerUps(Shrink, 8);
+        AddPowerUps(Grow, 8);
+        AddPowerUps(SlowTime, 6);
+        AddPowerUps(Ghost, 4);
+        AddPowerUps(ExtraLife, 3);
+        AddPowerUps(Nuke, 2);
+    }
+
+    private void AddPowerUps(GameObject powerUp, int amount)
+    {
+        for (int x = 0; x < amount; x++)
+        {
+            PowerUps.Add(powerUp);
+        }
+    }
+
+    private IEnumerator SpawnPowerUps()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+
+            GameObject powerUp = PowerUps[Random.Range(0, PowerUps.Count)];
+            Vector2 SpawnPos = new Vector2(Random.Range(minX, maxX), spawnY);
+            Instantiate(powerUp, SpawnPos, Quaternion.identity);
+
+            Debug.Log("Spawning power-up: " + powerUp.name + " at " + SpawnPos);
+
+        }
+    }
+}
